b05b2673c242a27c79f295576b4d46029156abb3
Gryzly <infocons@mail.ru>
2018-09-05 15:28:42 +0300

    Revert "Вступительное задание по Basejava."
    This reverts commit cad5f2b

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,22 +5,29 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
 
-    final private int MAX_COUNT = 10000;
+    private static final int MAX_COUNT = 10000;
+    private int sizeResume = 0;
     private Resume[] storage = new Resume[MAX_COUNT];
 
     void clear() {
         Arrays.fill(storage, null);
+        sizeResume = 0;
     }
 
     void save(Resume r) {
-        int sizeResume = size();
-      if (sizeResume<MAX_COUNT)  storage[sizeResume]=r;
+        if (sizeResume < MAX_COUNT) {
+            storage[sizeResume] = r;
+            ++sizeResume;
+        }
     }
 
     Resume get(String uuid) {
         int index = getIndex(uuid);
-        if (index>=0) return storage[index];
-        else return null;
+        if (index >= 0) {
+            return storage[index];
+        } else {
+            return null;
+        }
     }
 
     void delete(String uuid) {
@@ -28,38 +35,40 @@ public class ArrayStorage {
         int index = getIndex(uuid);
 
         // проверка на наличие элемента
-       if (index>=0) {
-           // сдвиг массива на элемент начиная с удаляемого элемента до первого null или до конца массива если весь массив заполнен
-           while (storage[index+1]!=null && index<MAX_COUNT-1){
-               storage[index]=storage[index+1];
-               index++;
-           }
-           storage[index] = null; // убрали последний элемент который уже сдвинут на предпоследнее место
-       }
+        if (index >= 0) {
+            // сдвиг массива на элемент начиная с удаляемого элемента до первого null или до конца массива если весь массив заполнен
+            while (storage[index + 1] != null && index < MAX_COUNT - 1) {
+                storage[index] = storage[index + 1];
+                index++;
+            }
+            storage[index] = null; // убрали последний элемент который уже сдвинут на предпоследнее место
+            --sizeResume;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-                 return Arrays.copyOf(storage, size());
-         }
+        return Arrays.copyOf(storage, sizeResume);
+    }
 
     // Вернет 0 если первый элемент null или колличество элементов до первого null
 
     int size() {
-        int i=0;
-        while (storage[i]!=null) i++;
+        int i = 0;
+        while (storage[i] != null) i++;
+        sizeResume = i;
         return i;
     }
 
+
     // Получаем индекс в массиве по тексту резюме если вхождения нет то возвращает -1
 
-     private int getIndex(String uuid) {
-        int index=-1;
-        int sizeR = size();
-        if (sizeR>=0) {
-            for (int i = 0; i<sizeR; i++) {
+    private int getIndex(String uuid) {
+        int index = -1;
+        if (sizeResume >= 0) {
+            for (int i = 0; i < sizeResume; i++) {
                 if (storage[i].getUuid().equals(uuid))
                     index = i;
             }
